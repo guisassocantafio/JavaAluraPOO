25ba1ca3ab3640b258cfc87c931921fc9168aea7
Gui <gsassocantafio@gmail.com>
2026-02-19 19:06:10 -0300

    aula01

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void main() {
+        Filme meuFilme = new Filme();
+        meuFilme.nome ="O Poderoso Chefão";
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.anoDeLancamento = 1970;
+
+        System.out.println(meuFilme.anoDeLancamento);
+    }
+}
